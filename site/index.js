87d77578e86fb3e4218d2d7f9b439e314b2cef07
Yogesh Joshi <yogeshjoshiy1@gmail.com>
2022-07-25 17:03:39 +0530

    complete

---
+++ site/index.js
@@ -0,0 +1,46 @@
+let hr = document.querySelector("#hr");
+        let mn = document.querySelector("#mn");
+        let sc = document.querySelector("#sc");
+
+        let hours = document.getElementById("hours");
+        let minutes = document.getElementById("minutes");
+        let seconds = document.getElementById("seconds");
+        let ampm = document.getElementById("ampm");
+
+        setInterval(()=>{
+            
+        let day = new Date();
+        let hh = day.getHours() * 30;
+        let mm = day.getMinutes() * 6;
+        let ss = day.getSeconds() * 6;
+
+        hr.style.transform = `rotateZ(${hh + (mm/12)}deg)`;
+        mn.style.transform = `rotateZ(${mm}deg)`;
+        sc.style.transform = `rotateZ(${ss}deg)`;
+
+        let h = day.getHours();
+        let m = day.getMinutes();
+        let s = day.getSeconds();
+
+        let am = h >= 12 ? "PM" : "AM";
+
+        if (h>12){
+            h = h - 12;
+        }
+
+        h = (h<10) ? "0"+ h : h;
+        m = (m<10) ? "0"+ m : m;
+        s = (s<10) ? "0"+ s : s;
+
+        hours.innerHTML = h;
+        minutes.innerHTML = m;
+        seconds.innerHTML = s;
+        ampm.innerHTML = am;
+
+        // if (h>12){
+        //     h = h - 12;
+        // }
+
+        // let am = h => 12 ? "PM" : "AM";
+
+        });
